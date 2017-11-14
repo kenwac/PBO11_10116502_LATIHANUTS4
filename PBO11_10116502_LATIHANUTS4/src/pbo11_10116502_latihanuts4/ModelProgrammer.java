a73c247b5f5f93a9d81fb0093bc8f1c6c6af9c32
kenwac <32459292+kenwac@users.noreply.github.com>
2017-11-14 20:10:41 +0700

    Add files via upload

---
+++ PBO11_10116502_LATIHANUTS4/src/pbo11_10116502_latihanuts4/ModelProgrammer.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11_10116502_latihanuts4;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ModelProgrammer extends ModelEmployee {
+    int bonus= 60000;
+    
+    public void BonusProgrammer(){
+        System.out.println("Salary Employee Programmer:"+salary);
+        System.out.println("Bonus Programmer:"+bonus);
+    }
+    
+}
---
+++ PBO11_10116502_LATIHANUTS4/src/pbo11_10116502_latihanuts4/ModelSystemAnalyst.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11_10116502_latihanuts4;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ModelSystemAnalyst extends ModelEmployee {
+    int bonus= 90000;
+    
+    public void BonusSystemAnalyst(){
+        System.out.println("Salary Employee System Analyst:" + salary);
+        System.out.println("Bonus System Analyst:"+bonus);
+    }
+    
+}
